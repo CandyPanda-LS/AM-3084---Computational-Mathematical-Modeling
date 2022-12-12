fcd14073d3d9ea2ce79bedafdb65d0bc9d5be45d
Lasal Sandeepa Hettiarachchi <62637475+CandyPanda-LS@users.noreply.github.com>
2022-12-12 11:02:56 +0530

    one big prime method

---
+++ Week3 - Modular Algorithms for Linear Algebra/OneBigPrime.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+# calculate the hadamard's upper bound for a determinant of a matrix
+def hadamard_bound(A):
+    #find the absolute value of the heighest value in the matrix
+    max = np.amax(np.absolute(A))
+    #find the degree of the matrix
+    n = np.size(A, 0)
+    H = n ** (n/2) * max ** n
+    return int(H)
+
+#create a function to check if a number is prime
+def is_prime(p):
+    for i in range(2, p):
+        if p % i == 0:
+            return False
+    return True
+
+
+#find a prime number that is greater than the two time hadamard's upper bound +1
+def find_prime(H):
+    #find the next prime number that is greater than 2*H
+    p = 2*H + 1
+    while True:
+        if is_prime(p):
+            return p
+        p += 1
+
+#take the element wise modulous of a matrix with a prime number
+def mod_matrix(A, p):
+    return np.mod(A, p)
+
+#find the determinent of a matrix
+def det(A):
+    return round(np.linalg.det(A))
+#find the bezout coefficients
+def bezout(a, b):
+    if b == 0:
+        return 1, 0
+    else:
+        x, y = bezout(b, a % b)
+        return y, x - y * (a // b)
+
+
+
+# # define a 2x2 matrix using np
+# A = np.array([[4, 5], [6, -7]])
+# h = hadamard_bound(A)
+# p = find_prime(h)
+# det_A_mod = det(mod_matrix(A,p))
+# print(det_A_mod)
+
+print(bezout(141,199))
